7404a3b2b48e9b2296cc4e80902d25fd3c044763
Dini Arisandi <92222142+diniiiiiiiiiii@users.noreply.github.com>
2023-06-26 23:13:27 +0700

    Delete config directory

--- config/database.js
+++
@@ -1,8 +0,0 @@
-const { Sequelize } = require("sequelize");
-
-const sequelize = new Sequelize("tbpweb", "root", "root", {
-  host: "localhost",
-  dialect: "mysql",
-});
-
-module.exports = sequelize;
